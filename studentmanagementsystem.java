b5fd5eb9d5fca19c4f0fedebcb2b04d5438ff261
dcole2004 <156231726+dcole2004@users.noreply.github.com>
2024-01-18 19:12:11 -0500

    Create studentmanagementsystem.java

---
+++ studentmanagementsystem.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Student
+{
+  public static void main(String[] args);
+  Scanner in = new Scanner(System.in);
+  System.out.println("Welcome to Cole Student Management System");
+  System.out.println("Do you want to add, remove or search a student?");
+  String response = in.next();
+
+public addStudent()
+  {
+  System.out.println("What is the name of the student");
+  String addName = in.next();
+  System.out.println("What is the age of the student");
+  int addAge = in.nextInt();
+  System.out.println("What is the GPA");
+  int addGPA = in.nextInt();
+  }
+public removeStudent()
+  {
+  System.out.println("Which is the name of the student you are looking to remove");
+  String remName = in.next();
+  }
