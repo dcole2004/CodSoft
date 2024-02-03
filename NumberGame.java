869d15eab3e0951bd2dce40e67dee79debd6578e
dcole2004 <156231726+dcole2004@users.noreply.github.com>
2024-02-03 15:30:27 -0500

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -3,24 +3,23 @@ import java.util.*;
 public class NumberGame {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int counter = 0;
-        int attempts = 5; // Assuming you have a certain number of attempts
-
         System.out.println("Hello, Welcome to Cole Number Game");
+        
+        // Generate a random number between 1 and 100 (inclusive)
+        int ran_num = (int) (Math.random() * 100) + 1;
+        
+        int attempts = 3;
 
         while (attempts > 0) {
-            int ran_num = (int) (Math.random() * 101);
-
             System.out.println("Please, pick a number between 1 and 100");
             int user_num = in.nextInt();
 
             if (user_num == ran_num) {
                 System.out.println("Congratulations, you guessed the right number");
-                counter++;
-                break; // Exit the loop if the user guessed correctly
+                break; // Exit the loop if the correct number is guessed
             } else if (user_num < ran_num) {
                 System.out.println("Not quite! Your number is too low, Try again!");
-            } else if (user_num > ran_num) {
+            } else {
                 System.out.println("Not quite! Your number is too high, Try again!");
             }
 
@@ -31,7 +30,6 @@ public class NumberGame {
             System.out.println("Out of attempts. The correct number was: " + ran_num);
         }
 
-        System.out.println("You guessed the right number in " + counter + " attempts.");
         in.close();
     }
 }
